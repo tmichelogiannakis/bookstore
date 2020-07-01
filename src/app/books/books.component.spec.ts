933cd6d0e0dda6bc1389d6fab109be08834b308e
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-01 19:01:42 +0300

    refactor: fix unit test

--- src/app/books/books.component.spec.ts
+++ src/app/books/books.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { BooksComponent } from './books.component';
 
 describe('BooksComponent', () => {
@@ -8,9 +8,9 @@ describe('BooksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BooksComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [BooksComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
