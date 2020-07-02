f41b18ba902b4785a61671f2b7cdb9806185d8d1
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-02 18:14:50 +0300

    feat: sidenav mode based on breakpoints

--- src/app/core/components/sidebar-container/sidebar-container.component.ts
+++ src/app/core/components/sidebar-container/sidebar-container.component.ts
@@ -1,14 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar-container',
   templateUrl: './sidebar-container.component.html',
-  styleUrls: ['./sidebar-container.component.scss']
+  styleUrls: ['./sidebar-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SidebarContainerComponent {
+  isLarge$: Observable<boolean>;
 
   @Input()
   opened: boolean;
 
-  constructor() {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.isLarge$ = this.breakpointObserver.observe(Breakpoints.Large).pipe(map((state) => state.matches));
+  }
 }
