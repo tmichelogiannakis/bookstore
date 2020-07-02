cde4de18a292a20791d27349bd6155ce2b3c1a83
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-02 19:53:01 +0300

    refactor: fix breaking tests

--- src/app/books/pages/add-book/add-book.component.spec.ts
+++ src/app/books/pages/add-book/add-book.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FormBuilder } from '@angular/forms';
 import { AddBookComponent } from './add-book.component';
 
@@ -9,7 +10,7 @@ describe('AddBookComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [AddBookComponent],
       providers: [FormBuilder]
     }).compileComponents();
