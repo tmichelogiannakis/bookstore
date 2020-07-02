c63251fcfea906bf632a30a9845b5ed7fd0e00b2
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-02 17:15:50 +0300

    feat: animate menu toggler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('menuButtonState', [
+      state(
+        'right',
+        style({
+          transform: 'rotate(0)'
+        })
+      ),
+      state(
+        'left',
+        style({
+          transform: 'rotate(-180deg)'
+        })
+      ),
+      transition('left => right', animate('250ms ease-in')),
+      transition('right => left', animate('250ms ease-out'))
+    ])
+  ]
 })
 export class AppComponent {
   opened: boolean = true;
