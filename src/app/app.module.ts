ecd19c2aa206353f2c10190a64d9df2ae0060da3
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-02 17:04:33 +0300

    feat: add sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { SidebarModule } from 'ng-sidebar';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, SidebarModule.forRoot(), CoreModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
