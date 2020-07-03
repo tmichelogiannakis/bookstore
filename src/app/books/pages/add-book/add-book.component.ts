40b4a9f39bebc55d06386028d06961b782251a9c
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-03 23:22:51 +0300

    refactor: replace min + max with range validator

--- src/app/books/pages/add-book/add-book.component.ts
+++ src/app/books/pages/add-book/add-book.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
-import { Subject } from 'rxjs';
-import { BooksService } from '../../services/books.service';
-import { takeUntil, filter } from 'rxjs/operators';
 import { ConfirmationService, SelectItem } from 'primeng/api';
+import { Subject } from 'rxjs';
+import { takeUntil, filter } from 'rxjs/operators';
+import { CustomValidators } from '../../../shared/custom.validators';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-add-book',
@@ -69,12 +70,16 @@ export class AddBookComponent implements OnInit, OnDestroy {
         Validators.pattern(`^(?!d+$)(?:[a-zA-Z0-9][a-zA-Z0-9 @"#&*!]*)?$`)
       ]),
       subtitle: this.fb.control(null, [
-        Validators.required,
         Validators.maxLength(120),
         // allow letters, digits and @"#&*! special chars up to 120 length
         Validators.pattern(`^(?!d+$)(?:[a-zA-Z0-9][a-zA-Z0-9 @"#&*!]*)?$`)
       ]),
-      description: this.fb.control(null, [Validators.required, Validators.maxLength(512)]),
+      description: this.fb.control(null, [
+        Validators.required,
+        Validators.maxLength(512),
+        // must start with capital letter
+        Validators.pattern(`^[A-Z].*`)
+      ]),
       categories: this.fb.control([]),
       authors: this.fb.control([], [Validators.required]),
       isbn: this.fb.control(null, [
@@ -90,7 +95,7 @@ export class AddBookComponent implements OnInit, OnDestroy {
       // validate numeric value up to 9999
       pages: this.fb.control(null, [Validators.required, Validators.pattern('^[0-9]{1,4}$')]),
       // validate from 1900 until today
-      published: this.fb.control(null, [Validators.required, Validators.min(1900), Validators.max(dt.getFullYear())]),
+      published: this.fb.control(null, [Validators.required, CustomValidators.range(1900, dt.getFullYear())]),
       publisher: this.fb.control(null, [Validators.required, Validators.minLength(5), Validators.maxLength(60)]),
       // validate website address
       website: this.fb.control(null, Validators.pattern(`(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?`))
