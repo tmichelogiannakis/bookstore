76f17d10c06afb9eea9f13640bd9526bb04aa2ee
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-04 00:52:11 +0300

    refactor: fix unit tests

--- src/app/books/pages/add-book/add-book.component.spec.ts
+++ src/app/books/pages/add-book/add-book.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { AddBookComponent } from './add-book.component';
 
@@ -12,7 +13,8 @@ describe('AddBookComponent', () => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [AddBookComponent],
-      providers: [FormBuilder]
+      providers: [FormBuilder],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
