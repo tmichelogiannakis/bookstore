0263e852ba4b6eeb7fe22691f5139c7eda7217c4
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-03 18:30:28 +0300

    build: import chips

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { ChipsModule } from 'primeng/chips';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { DataViewModule } from 'primeng/dataview';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputTextModule } from 'primeng/inputtext';
 
-const modules = [ButtonModule, ConfirmDialogModule, DataViewModule, InputTextModule, InputTextareaModule];
+const modules = [ButtonModule, ChipsModule, ConfirmDialogModule, DataViewModule, InputTextModule, InputTextareaModule];
 
 @NgModule({
   declarations: [],
