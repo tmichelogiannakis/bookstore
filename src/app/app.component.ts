c780e2a5eeb134c5bbad2ccb4699eae06429ce0a
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-04 20:34:25 +0300

    feat: add breadcrumb on app comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { BreadcrumbService } from './core/services/breadcrumb.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   opened: boolean = true;
+
+  breadcrumbItems: MenuItem[];
+
+  constructor(private breadcrumbService: BreadcrumbService, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.breadcrumbService.breadcrumbItems$.subscribe((breadcrumbItems) => {
+      this.breadcrumbItems = breadcrumbItems;
+      this.cdr.detectChanges();
+    });
+  }
 
   onToggleSidebar() {
     this.opened = !this.opened;
