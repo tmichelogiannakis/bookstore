59177fe0d5480879d5748174d9641a039a03a09e
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-05 12:23:30 +0300

    feat: create a file upload form component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,10 +4,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { PrimengModule } from './primeng/primeng.module';
 import { ArrayPipe } from './pipes/array.pipe';
+import { FileuploadComponent } from './components/form/fileupload/fileupload.component';
+
+const components = [FileuploadComponent];
 
 @NgModule({
-  declarations: [ArrayPipe],
+  declarations: [...components, ArrayPipe],
   imports: [CommonModule, ReactiveFormsModule, RouterModule, PrimengModule],
-  exports: [CommonModule, ReactiveFormsModule, RouterModule, PrimengModule, ArrayPipe]
+  exports: [CommonModule, ReactiveFormsModule, RouterModule, PrimengModule, ...components, ArrayPipe]
 })
 export class SharedModule {}
