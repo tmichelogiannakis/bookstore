99059128660d6138772d33831a984a5b1d98d726
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-06 21:33:25 +0300

    build: import dropdown

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -3,14 +3,12 @@ import { BlockUIModule } from 'primeng/blockui';
 import { ButtonModule } from 'primeng/button';
 import { ChipsModule } from 'primeng/chips';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DropdownModule } from 'primeng/dropdown';
 import { FileUploadModule } from 'primeng/fileupload';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { InputTextModule } from 'primeng/inputtext';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-
-
-
 import { SliderModule } from 'primeng/slider';
 
 const modules = [
@@ -18,6 +16,7 @@ const modules = [
   BlockUIModule,
   ChipsModule,
   ConfirmDialogModule,
+  DropdownModule,
   FileUploadModule,
   InputTextModule,
   InputTextareaModule,
