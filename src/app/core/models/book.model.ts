0e2ca4f5f1ca774beae2d879709d1c808982f64a
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-06 18:03:10 +0300

    feat: book published to year

--- src/app/core/models/book.model.ts
+++ src/app/core/models/book.model.ts
@@ -11,3 +11,13 @@ export interface Book {
   website: string;
   image?: string;
 }
+
+// published is either numeric (year) or string (date)
+// if not numeric take the Year part
+export const formatBook = (book: Book): Book => {
+  let published = book.published;
+  if (isNaN(Number(published))) {
+    published = new Date(published).getFullYear();
+  }
+  return { ...book, published };
+};
