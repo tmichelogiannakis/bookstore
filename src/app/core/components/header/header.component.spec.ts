d33d38f80cf3182b9ad8f9603d4edb8f99a8dfa4
Haris Michelogiannakis <tmichelogiannakis@gmail.com>
2020-07-07 19:04:31 +0300

    test: add header comp unit tests

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,18 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { HeaderComponent } from './header.component';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<app-header [sidebarOpened]="sidebarOpened" (toggleSidebar)="onToggleSidebar()"></app-header>`
+})
+class HostComponent {
+  sidebarOpened: boolean = true;
+
+  onToggleSidebar() {
+    this.sidebarOpened = !this.sidebarOpened;
+  }
+
+  setSidebarOpened(value: boolean) {
+    this.sidebarOpened = value;
+  }
+}
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let hostComponent: HostComponent;
+  let hostFixture: ComponentFixture<HostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, NoopAnimationsModule],
-      declarations: [HeaderComponent],
-      schemas: [NO_ERRORS_SCHEMA]
+      declarations: [HeaderComponent, HostComponent]
     }).compileComponents();
   }));
 
@@ -20,9 +37,34 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    hostFixture = TestBed.createComponent(HostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostFixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit to toggle sidebar', () => {
+    spyOn(component.toggleSidebar, 'emit');
+    const button = fixture.debugElement.query(By.css('.menu-button')).nativeElement;
+    button.click();
+    expect(component.toggleSidebar.emit).toHaveBeenCalledWith();
+  });
+
+  it('supports the sidebarOpened attribute as binding', () => {
+    const nestedComponent: HeaderComponent = hostFixture.debugElement.query(By.directive(HeaderComponent)).componentInstance;
+
+    // set sidebaOpened to true on host component and check on if true on test component
+    hostComponent.setSidebarOpened(true);
+    hostFixture.detectChanges();
+    expect(nestedComponent.sidebarOpened).toBe(true);
+
+    // set sidebaOpened to false on host component
+    hostComponent.setSidebarOpened(false);
+    hostFixture.detectChanges();
+    expect(nestedComponent.sidebarOpened).toBe(false);
   });
 });
